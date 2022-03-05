6691a7f2ea5f8c9f30f46eddf3e3eea8c9dae453
Sandeep Singh <43696323+sandeepsingh96@users.noreply.github.com>
2022-03-05 12:04:00 -0700

    error solved

--- src/component/Products.js
+++ src/component/Products.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Skeleton from "react-loading-skeleton";
 const Products = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
   const [filter, setFilter] = useState(data);
   const [loading, setLoading] = useState(false);
   let componentMounted = true;
